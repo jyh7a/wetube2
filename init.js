f763cfbe303aaba6118137d24aee6470352c59f3
YoungHun Jeong <jyh7a@naver.com>
2020-09-24 00:21:26 +0900

    video model update
    video model
    https://nomadcoders.co/wetube/lectures/39

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 import "./db"
 import app from './app'
+import dotevn from "dotenv"
+dotevn.config()
+import "./models/Video"
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`)
 
